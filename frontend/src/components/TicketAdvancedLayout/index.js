cb20ca5a36527aec9f9bad6a94eb59ee978f2cd4
Luis Filipe Gomes <eu.gdcarvalho@icloud.com>
2023-03-30 19:00:52 -0300

    Inital Commit

---
+++ frontend/src/components/TicketAdvancedLayout/index.js
@@ -0,0 +1,10 @@
+import { styled } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const TicketAdvancedLayout = styled(Paper)({
+    height: `calc(94vh)`,
+    display: "grid",
+    gridTemplateRows: "0px 1fr"
+})
+
+export default TicketAdvancedLayout;
